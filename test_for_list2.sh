0a17a11e69b7cf5169b0919970f9a53e02c7a7c9
andrey_p <andrey_p@najet.ru>
2021-04-01 14:48:33 +0300

    add new file test for list2

---
+++ test_for_list2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#list="One\ntwo\nthree\nfour"
+
+#Print the list with echo
+#echo -e "echo: \n$list"
+
+#Set the field separator to new line
+#IFS=$'\n'
+
+#Try to iterate over each line
+#echo "For loop:"
+#for item in $list
+#do
+#        echo "Item: $item"
+#done
+
+#Output the variable to a file
+#echo -e $list > list.txt
+
+#Try to iterate over each line from the cat command
+#echo "For loop over command output:"
+#for item in `cat list.txt`
+#do
+#        echo "Item: $item"
+#done
+#echo ""
+#for itemm in $list
+#do
+#        echo "Itemm: $itemm"
+#done
+
+
+
+list="One\ntwo\nthree\nfour"
+echo "$list"
+#One\ntwo\nthree\nfour
+list=$'One\ntwo\nthree\nfour'
+echo "$list"
+#One
+#two
+#three 
+#four
+#И если у вас уже есть такая строка в переменной, вы можете читать ее построчно с помощью:
+
+while read -r $line; do
+    echo "... $line ..."
+	echo ""
+
+	if [ -d "$line" ]
+		then
+		echo "Check folder - $line succesfully!"
+	else
+		echo "Folder $1 not exist. Create..."
+		mkdir -p /home/andrew/1c/$line
+	fi
+done <<< "$list"
